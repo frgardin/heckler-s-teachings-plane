cfdaeab403cfdf943c2dad91ae649adce9790f3d
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-02-21 21:48:23 -0300

    creating plane repo

---
+++ src/test/java/net/planefinder/heckler/spring/study/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.planefinder.heckler.spring.study.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
